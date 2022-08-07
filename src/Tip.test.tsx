f584906fe12af3fb3c2dc24ce4317a4da266e983
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-08-06 18:24:43 -0700

    feat: compile with vite and vitest

--- src/Tip.test.tsx
+++ src/Tip.test.tsx
@@ -1,7 +1,8 @@
-import {round} from './Tip';
+import { describe, it, expect } from "vitest";
+import { round } from "./Tip";
 
-describe('round', () => {
-  it('rounds correctly to nearest 2 decimals', () => {
+describe("round", () => {
+  it("rounds correctly to nearest 2 decimals", () => {
     expect(round(6.774)).toEqual(6.77);
     expect(round(6.775)).toEqual(6.78);
     expect(round(1.005)).toEqual(1.01);
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  build: { outDir: "build" },
+  plugins: [react()],
+});
